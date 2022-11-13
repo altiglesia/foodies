3c547e6cada40105b5e1ed15746e347472aa9526
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-11-12 19:31:09 -0500

    fixed main

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,24 +3,16 @@ import Home from "./Home";
 import Scroll from "./Scroll";
 import Likes from "./Likes";
 import Friends from "./Friends";
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function Main() {
-    return(
-        <>
-            <Route path="/">
-                <Home />
-            </Route>
-            <Route path="/scroll">
-                <Scroll />
-            </Route>
-            <Route path="/likes">
-                <Likes />
-            </Route>
-            <Route path="/friends">
-                <Friends />
-            </Route>
-        </>
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/scroll" element={<Scroll />} />
+            <Route path="/likes" element={<Likes />} />
+            <Route path="/friends" element={<Friends />} />
+        </Routes>
     );
 }
 
