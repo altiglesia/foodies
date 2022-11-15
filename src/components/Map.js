d9cce83bf112e18dbd2e2ca593c93a6ebb4ae78a
Chris Li <chris.lty07@gmail.com>
2022-11-15 13:51:17 -0500

    Imported Leaflet and Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 function Map() {
-    return(
-        <div>
-            <h1>"Map goes here!"</h1>
+    // const position = [40.706027, -74.008835]
+    return (
+        <div id="map">
+            <h3>Map goes here</h3>
+
+            <MapContainer center={[40.706027, -74.008835]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {/* <Marker position={[40.706027, -74.008835]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker> */}
+            </MapContainer>
         </div>
     )
 }
