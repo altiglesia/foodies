5ae0ab2c1ae6604c11ef17b53c3e5a9a7fd67447
Chris Li <chris.lty07@gmail.com>
2022-11-15 14:53:27 -0500

    Added leaflet maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,36 @@
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
-function Map() {
-    // const position = [40.706027, -74.008835]
+function Map({ restaurantData }) {
+
+
+
     return (
         <div id="map">
-            <h3>Map goes here</h3>
+            <h3>Where have your friends been judging?</h3>
 
             <MapContainer center={[40.706027, -74.008835]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {/* <Marker position={[40.706027, -74.008835]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker> */}
+                {restaurantData.map(restaurant => {
+                    const coordinates = [restaurant.latitude, restaurant.longitude]
+                    const imageLink = restaurant.restaurant_images[0] ? restaurant.restaurant_images[0]["image_url"] : null;
+                    // debugger
+                    return (
+                        <Marker key={restaurant.id} position={coordinates}>
+                            <Popup>
+                                <h4>{restaurant.name}</h4>
+                                <p>{restaurant.address}, {restaurant.neighborhood}</p>
+                                {/* <img src={imageLink}></img> */}
+                            </Popup>
+                        </Marker>
+                    )
+                })}
+                <Marker position={[40.706027, -74.008835]}>
+
+                </Marker>
             </MapContainer>
         </div>
     )
