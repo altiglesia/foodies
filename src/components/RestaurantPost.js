305a57c8189ed7d7693248761adf10714c1e8f9b
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-11-15 20:05:22 -0500

    comments section

--- src/components/RestaurantPost.js
+++ src/components/RestaurantPost.js
@@ -7,9 +7,27 @@ function RestaurantPost({ restaurant, saveFaveRestaurant }) {
         console.log(restaurant)
     }
 
+    // function handleSubmit(e) {
+    //     e.preventDefault()
+    //     fetch(`http://localhost:9292/${restaurant.id}`, {
+    //         method: "PATCH",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Accept: "application/json",
+    //         },
+    //         body: JSON.stringify({
+
+    //         })
+    //     })
+    // }
+
+    function handleChange(e) {
+        console.log(e.target.value)
+    }
+
     return (
         <div className="restaurant-post">
-            <h3>{restaurant.name}</h3>
+            <h3 id="restaurant-name">{restaurant.name}</h3>
             <img src={restaurant.images[0]} />
             {/* {restaurant.images[0].slice(0,1).map(image => 
                 <img 
@@ -17,10 +35,14 @@ function RestaurantPost({ restaurant, saveFaveRestaurant }) {
                     alt="restaurant pics"
                     key={image.id} />
             )} */}
-            <h4>{restaurant.neighborhood}</h4>
+            <h4 id="neighborhood">{restaurant.neighborhood}</h4>
+            <h5 id="address">{restaurant.address}</h5>
             <button id="dislike">❌</button>
             <button id="fave" onClick={saveFaveRestaurantClick}>⭐️</button>
             <button id="like">❤️</button>
+            <form id="comments-section" onSubmit={handleSubmit}>
+                <input type="text" name="comments" placeholder="leave a comment" onChange={handleChange}></input>
+            </form>
         </div>
     )
 }
