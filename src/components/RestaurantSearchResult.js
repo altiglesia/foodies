37967bfd2aa127b7e559ab5c87b0455ad1f73bd4
Chris Li <chris.lty07@gmail.com>
2022-11-16 18:54:33 -0500

    Added total likes and dislikes

--- src/components/RestaurantSearchResult.js
+++ src/components/RestaurantSearchResult.js
@@ -1,8 +1,10 @@
 function RestaurantSearchResult({ restaurant }) {
+    // debugger
     return (
         <div className="restaurantResultContainer">
             <h3>{restaurant.name}</h3>
             <p>{restaurant.address}</p><span>{restaurant.neighborhood}</span>
+
             {/* <img src={restaurant.images[2].replace("500x500", "100x100")} /> */}
 
 
