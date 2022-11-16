bf18d459c71590b2939c62a5db0867a1effb16d5
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-11-16 07:38:12 -0500

    comments section

--- src/components/RestaurantPost.js
+++ src/components/RestaurantPost.js
@@ -3,7 +3,8 @@ import ImageGallery from 'react-image-gallery';
 
 function RestaurantPost({ restaurant, saveFaveRestaurant }) {
     // add button click events (must look at Sinatra notes to update DB reviews_table from frontend event)
-    const [comment, setComment] = useState("")
+    const [commentData, setCommentData] = useState("")
+    const [isPending, setIsPending] = useState(false);
 
     function saveFaveRestaurantClick() {
         saveFaveRestaurant(restaurant)
@@ -12,26 +13,31 @@ function RestaurantPost({ restaurant, saveFaveRestaurant }) {
 
     // post new review to reviews in backend
     function handleSubmit(e) {
-        e.preventDefault()
-        console.log(e.target.value)
+        e.preventDefault();
+
+        setIsPending(true)
         // fetch(`http://localhost:9292/restaurant/${restaurant.id}/reviews`, {
         //     method: "POST",
         //     headers: {
-        //         "Content-Type": "application/json",
-        //         Accept: "application/json",
-        //     },
+        //         "Content-Type": "application/json"},
         //     body: JSON.stringify({
         //         restaurant_id: restaurant.id,
         //         user_id: 1,
-        //         review_detail_comment: e.target
+        //         review_detail_comment: e.target.value
         //     })
         // })
-        // .then((r) => r.json())
+        .then(() => {
+            console.log('new comment');
+            setIsPending(false)
+        })
         // .then((newComment) => onAddComment(newComment));
     }
 
     function handleChange(e) {
-        console.log(e.target.value)
+        setCommentData({
+            username: e.target.name,
+            comment: e.target.value,
+        })
     }
 
     const imageGallery = restaurant.images.map(image => {
@@ -44,16 +50,32 @@ function RestaurantPost({ restaurant, saveFaveRestaurant }) {
 
     return (
         <div className="restaurant-post">
+
             <h3>{restaurant.name}</h3>
+
             <ImageGallery items={imageGallery} />
+
             <h4>{restaurant.neighborhood}</h4>
+
             <button id="dislike">👎</button>
+
             <button id="fave" onClick={saveFaveRestaurantClick}>⭐️</button>
+
             <button id="like">❤️</button>
+
+            <ul>
+                {Object.entries(commentData).map(([username, commentValue]) => (
+                    <li key={username}>{commentValue.toString()}</li>
+                ))}
+            </ul>
+
             <form id="comments-section" onSubmit={handleSubmit}>
                 <input type="text" name="comments" placeholder="leave a comment" onChange={handleChange}></input>
+                <input type="name" name="username" placeholder="username" onChange={handleChange}></input>
             </form>
-            <button type="submit" form="comments-section">post</button>
+            { !isPending && <button type="submit" form="comments-section">post</button> }
+            { isPending && <button type="submit" form="comments-section">posting comment...</button> }
+        
         </div>
     )
 }
